09bcdbd95a1223492f276d39c3f73eb237d304e8
FajarRizkycode <fajar.andiansyah24@student.uisi.ac.id>
2026-05-31 14:55:39 +0700

    add function counter

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function toggledark(){
     document.body.classList.toggle("bg-dark");
     document.body.classList.toggle("text-white");
 }
+
+function counter(id, max) {
+  let i = 0;
+  let interval = setInterval(() => {
+    document.getElementById(id).innerText = i;
+    i += 10;
+    if (i >= max) clearInterval(interval);
+  }, 30);
+}
