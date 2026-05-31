28982c5eba44ce6ac5ed65c163a7903cc40f37cf
FajarRizkycode <fajar.andiansyah24@student.uisi.ac.id>
2026-05-31 15:02:39 +0700

    add items foreach filter Gallery

--- script.js
+++ script.js
@@ -36,5 +36,19 @@ team.forEach(t => {
 
 function filterGallery(category) {
   let items = document.querySelectorAll(".gallery-item");
-  
+
+    items.forEach(item => {
+    if (category === "all") {
+      item.classList.remove("hide");
+    } else {
+      if (item.classList.contains(category)) {
+        item.classList.remove("hide");
+      } else {
+        item.classList.add("hide");
+      }
+    }
+  });
+
+
 }
+
